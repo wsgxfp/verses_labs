d4da40ce5d766d97f2f53dbb5f32bd38035eca6b
wsgxfp <151615336+wsgxfp@users.noreply.github.com>
2024-03-08 12:31:59 +0200

    Update lab22.py

--- lab22.py
+++ lab22.py
@@ -10,15 +10,32 @@ def find_sum(M, P):
             if curr_sum == P:
                 return True
             elif curr_sum < P:
-                left += 1
+                if search_third(M, left, right, P - M[i] - M[left]):
+                    return True
+                else:
+                    left += 1
             else:
                 right -= 1
     return False
 
+
+def search_third(arr, left, right, target):
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return False
+
+
 M = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-P = 11
+P = 16
 
 if find_sum(M, P):
     print("Такі числа є")
 else:
     print("Таких чисел немає")
+
