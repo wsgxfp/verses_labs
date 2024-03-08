53448606f8411a58a70422a0d5ba2ac1afe30e0d
wsgxfp <151615336+wsgxfp@users.noreply.github.com>
2024-03-08 12:33:25 +0200

    Update test_lab22.py

--- test_lab22.py
+++ test_lab22.py
@@ -1,14 +1,29 @@
 import unittest
 
-from lab22 import find_sum
+from lab2 import find_sum
 
 
-class TestFindTripletSum(unittest.TestCase):
+class TestFindSum(unittest.TestCase):
+    def test_existence(self):
+        M = [100, 225, 356, 4221, 625, 906, 1437, 821, 950, 1000]
+        P = 681
+        self.assertTrue(find_sum(M, P))
 
-    def test_example_1(self):
-        arr = [1, 2, 3, 6]
-        P = 6
-        self.assertTrue(find_sum(arr, P))
+    def test_nonexistence(self):
+        M =[100, 225, 356, 4221, 625, 906, 1437, 821, 950, 1000]
+        P = 100
+        self.assertFalse(find_sum(M, P))
 
-if __name__ == "__main__":
+    def test_specific_elements(self):
+        M = [100, 225, 356, 4221, 625, 906, 1437, 821, 950, 1000]
+        P = 1675
+        self.assertTrue(find_sum(M, P))
+
+    def test_another_combination(self):
+        M = [100, 225, 356, 4221, 625, 906, 1437, 821, 950, 1000]
+        P = 2131
+        self.assertTrue(find_sum(M, P))
+
+
+if __name__ == '__main__':
     unittest.main()
